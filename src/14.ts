0976297f2b3cf54e509785ba6b223870b9db759c
Ryota Kojima <ryotakojima@RyotanoMacBook-Pro.local>
2025-06-07 15:44:41 +0900

    wip: 15

--- src/14.ts
+++ src/14.ts
@@ -1,11 +1,52 @@
-export function getHummer(): Hummingbird {
-  interface Hummingbird {
-    name: string
-    weightGrams: number
-  }
+// export function getHummer(): Hummingbird {
+//   interface Hummingbird {
+//     name: string
+//     weightGrams: number
+//   }
 
-  const bee: Hummingbird = {name: 'bee', weightGrams: 2.5}
-  return bee
+//   const bee: Hummingbird = {name: 'bee', weightGrams: 2.5}
+//   return bee
+// }
+
+// getHummer()
+
+type  hoge = Readonly<number[]> 
+type other  = readonly number[]
+
+export {}
+
+const  printTriangle = (n: number) => {
+  const numbers = []
+  for (let i = 0; i < n; i++) {
+    numbers.push(i)
+    console.log(arraySum(numbers as hoge))
+  }
 }
 
-getHummer()
+// function arraySum(arr: other) {
+//   let sum = 0, num;
+//   while ((num = arr.pop()) !== undefined) {
+//     sum += num 
+//   }
+
+//   return sum
+// }
+
+
+function arraySum(arr: readonly number[]) {
+  let sum = 0
+  for (const num of arr) {
+    sum += num
+  }
+  return sum
+}
+
+printTriangle(5)
+
+
+let piyo : readonly number[] = [0]
+
+
+piyo[1] = 20
+
+console.log(piyo)
---
+++ src/15.ts
@@ -0,0 +1,64 @@
+export {}
+
+type CylinderFn = (r: number, h: number) => number
+
+const surfaceArea: CylinderFn = (r, h) => 2 * Math.PI * r * (r + h)
+const valume: CylinderFn = (r, h) => Math.PI * r * r * h
+
+const arr: Array<[number, number]> = [
+  [1, 1],
+  [1, 2],
+  [2, 1],
+]
+
+arr.map(([r, h]) => {
+  console.log(`cylinder:  r=${r} x h=${h}`)
+  console.log(`surface area:  ${surfaceArea(r, h)}`)
+  console.log(`valume:  ${valume(r, h)}`)
+})
+
+interface Vertebrate {
+  weightGrams: number
+  color: string
+  isNocturnal: boolean
+}
+
+interface Bird extends Vertebrate {
+  wingspanCm: number
+}
+
+interface Mammal extends Vertebrate {
+  eatsGardenPlants: boolean
+}
+
+type hoge = 'hohgoehoge' | 'fugafugafauga'
+
+type fuga = 'fasdfadsf' | hoge
+
+interface State {
+  userId: string
+  pageTitle: string
+  recentFiles: string
+  pageContents: string
+}
+
+// interface TopnavState {
+//   userId: State['userId']
+// }
+
+type TopNavState = {
+  [K in 'userId']: State[K]
+}
+type Pick<T, K> = { [k in K]: T[k] }
+
+
+
+interface Save {
+  type: 'save'
+}
+interface Load {
+  type : 'load'
+}
+type Action = Save | Load
+type ActionType = Action['type']
+type OtherType = Pick<Action, 'type'>
